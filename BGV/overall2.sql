85ccc31dd4aafe7532b509d492706753f02ba1ea
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-08-14 00:18:38 +0530

    Create overall2.sql

---
+++ BGV/overall2.sql
@@ -0,0 +1,8 @@
+select onboarding_biz,count (distinct mobile) as total_drivers,
+count(distinct case when final_status='Verified' then mobile end) as final_verification_count,
+count(distinct case when final_status='Failed' then mobile end) as final_failed_count,
+count(distinct case when final_status='Inprocess' then mobile end) as final_inprocess_count
+from tmp_operations.bgv_data b
+left join tmp_operations.vehicle_consolidated v on b.vbn=v.vbn
+where onboarding_biz<>'' and onboarding_biz<>'ONBOARDING CHURN' and verification_subsription_category='ID+CRC+ADD'
+Group by 1 
