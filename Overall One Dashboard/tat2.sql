da65577a9b0960c105d5a0fb2cc36ba15e2e5eb5
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-08-23 12:07:11 +0530

    Create tat2.sql

---
+++ Overall One Dashboard/tat2.sql
@@ -0,0 +1,25 @@
+select case when months = 6 then 'June'
+when months = 7 then 'July' 
+when months = 5 then 'May'
+when months = 4 then 'April'
+when months = 8 then 'August' end as months,total_vbn,
+round((zero_to_2_days*100.0/total_vbn),2)||'%' zero_to_2_days
+,round((three_to_8_days*100.0/total_vbn),2)||'%' three_to_8_days
+,round((nine_to_15_days*100.0/total_vbn),2)||'%' nine_to_15_days
+,round((greater_than_15*100.0/total_vbn),2)||'%' greater_than_15
+from
+(
+select extract(months from ob_date) months,count(distinct vbn) as total_vbn,count(distinct case when tat between 0 and 2 then vbn end) as zero_to_2_days,
+count(distinct case when tat between 3 and 8 then vbn end) as three_to_8_days,
+count(distinct case when tat between 9 and 15 then vbn end) as nine_to_15_days,
+count(distinct case when tat >= 16 then vbn end) as greater_than_15
+from
+(
+Select date(onboarding_date) as ob_date,vehicle_consolidated.vbn,vc_last_call_date,(least(first_ride,first_delivery_date,first_swap_date)-date(onboarding_date)) tat
+from tmp_operations.vehicle_consolidated
+left join tmp_operations.zoho_crm_data on vehicle_consolidated.driver_mobile=zoho_crm_data.driver_phone_number
+where onboarding_date between '2021-04-01' and last_day(current_date)
+) as a
+group by 1
+order by 1
+) as a
