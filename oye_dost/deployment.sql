d086b08c18b5ff05c507275d257f17722d99511e
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-09-08 12:19:57 +0530

    Create deployment.sql

---
+++ oye_dost/deployment.sql
@@ -0,0 +1,14 @@
+Select v.onboarding_date, count(DISTINCT CASE 
+			WHEN referral_vbn IS NOT NULL
+				THEN referral_mobile
+			END) AS O2D_VBN,count(distinct case when (last_ride between '2021-09-01' and current_date) OR (last_delivery_date between '2021-09-01' and current_date) OR
+			(last_swap_date between '2021-09-01' and current_date) then referral_vbn end) as Active_o2D
+			,count(distinct case when (lifetime_rides between 1 and 5)or (total_delivery_days between 1 and 5) OR (total_swaps between 1 and 5) then vbn end)as One_five
+						,count(distinct case when (lifetime_rides>5)or (total_delivery_days>5) OR (total_swaps>5) then vbn end)as More_than_five
+
+			from tmp_operations.oye_dost mp
+			left join tmp_operations.referrals_lead_data rl on mp.mobile_number=rl.referree_mobile
+			left join tmp_operations.vehicle_consolidated v on v.driver_mobile=rl.referral_mobile
+			where v.onboarding_date between '2021-09-01' and current_date
+			group by 1 
+			order by 1
